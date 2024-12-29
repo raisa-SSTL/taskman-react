9a47d05ecac34f0229ee06b07bee3bdb875bd7d8
raisa-SSTL <raisa@synergysailtech.com>
2024-12-29 22:05:20 +0600

    login page is displayed first after server starts

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -40,7 +40,8 @@ const ThemeRoutes = [
     path: "/",
     element: <FullLayout />,
     children: [
-      { path: "/", element: <Navigate to="dashboards/dashboard1" /> },
+      // { path: "/", element: <Navigate to="dashboards/dashboard1" /> },
+      { path: "/", element: <Navigate to="login" /> },
       { path: "dashboards/dashboard1", exact: true, element: <Dashboard1 /> },
       { path: "tables/basic-table", element: <BasicTable /> },
       { path: "/form-layouts/form-layouts", element: <FormLayouts /> },
